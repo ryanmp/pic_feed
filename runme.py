28d44ab0b8e297e93c2037e2c7690e34490bb25e
Ryan Phillips <wordsandcolor@gmail.com>
2014-06-25 15:02:37 -0700

    auto resize and compress src images

--- runme.py
+++ runme.py
@@ -1,4 +1,4 @@
-import os, jinja2, time, datetime, math
+import os, jinja2, time, datetime, math, Image
 
 def main():
 	out = get_imgs()
@@ -11,26 +11,38 @@ def get_imgs():
 	ret_ext = []
 	fdates = []
 	datetimes = []
-	all_files = os.listdir(path+'/img')
+	all_files = os.listdir(path+'/src_images')
 	for f in all_files:
 		if f.endswith(('.png', '.jpg','.jpeg','.gif')):
 
-			creation_time = os.stat(path+'/img/'+f).st_mtime
+			# get datetime
+			creation_time = os.stat(path+'/src_images/'+f).st_mtime
 			as_datetime = datetime.datetime.strptime(time.ctime(creation_time), "%a %b %d %H:%M:%S %Y")
 			datetimes.append(as_datetime)
 			fdate = as_datetime.strftime("%B %e, %Y")
 			fdates.append(fdate)
 			
+			# file name and extension
+			## kind of unnecessary now since I'm converting all of them to jpg
 			tmp = f.split('.')
 			ret_name.append(tmp[0])
 			ret_ext.append(tmp[1])
+
+			# create compressed version... store in '/imgs'
+			img = Image.open(path+'/src_images/'+f)
+
+			if (img.size[0] > 810):
+				img = img.resize((810,int(810*(float(img.size[1])/img.size[0]))), Image.ANTIALIAS)
+
+			img.save(path+'/imgs/'+tmp[0]+'.jpg','JPEG', dpi=[300,300], quality=80)
+
 	return [ret_name, ret_ext, fdates, datetimes]
 
 
 def render_template(_names, _exts, _dates):
 
 	total_images = len(_names)
-	images_per_page = 5
+	images_per_page = 10
 	num_pages = int(math.ceil(float(total_images)/images_per_page))
 
 	# generate list of html pages to be rendered
